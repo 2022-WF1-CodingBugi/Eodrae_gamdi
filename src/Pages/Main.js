8c26aac2d308d12a949a243329c2c2354c3ee704
chanjoo <wcj0911@naver.com>
2022-11-11 12:36:30 +0900

    캐러셀 폰트 수정

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -21,7 +21,7 @@ const Main = ({ onChange = f => f }) => {
         return (
             <div className="carousel_div" onClick={() => window.open(data.kakao_map)}>
                 <img className="carousel_img" src={data.image} alt={data.name}/>
-                <h5>{data.name}</h5>
+                <h4 className="carousel_name">{data.name}</h4>
             </div>
         )
     }
@@ -31,7 +31,7 @@ const Main = ({ onChange = f => f }) => {
             <header id="header">
                 <img id="main_logo" src={main_logo} alt="어드레감디" />
             </header>
-            <section style={{marginTop: "50px", marginBottom: "100px"}}>
+            <section style={{marginTop: "80px", marginBottom: "130px"}}>
                 <Category name="food" src={food_category} onChangePage={onChange} />
                 <Category name="activity" src={activity_category} onChangePage={onChange} />
                 <Category name="lodging" src={lodging_category} onChangePage={onChange} />
--- src/Pages/Sub.js
+++ src/Pages/Sub.js
@@ -26,9 +26,7 @@ const Sub = () => {
     return (
         <>
             <header>
-                
                 <img onClick={()=>{navigate('/')}} id="logo" src={main_logo} alt="어드레감디"/>
-                
             </header>
             <MapDiv />
         </>
