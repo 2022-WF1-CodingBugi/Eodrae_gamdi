86e62ffc175b8fed700f276833693b70af03d2c8
chanjoo <wcj0911@naver.com>
2022-11-17 12:29:34 +0900

    좋아요 위치, 리스트 크기 수정

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -17,6 +17,13 @@ function List({ places, setPlace }) {
   const [isChecked, setChecked] = useState(checkedArray); // 좋아요 버튼이 클릭 됐는지
   const [likes, setLikes] = useState( likesArray ); // 좋아요 배열 상태 변수로
 
+  const writeDataFile =  (fileName, data) => {
+    if(fileName === "") return false;
+
+    const content = {data};
+    console.log(content);
+  }
+
   // i번째 리스트가 클릭 됐을 때
   const toggleLike = (event, i) => {
     // 상태 변수에 있는 배열 복사
@@ -73,10 +80,11 @@ function List({ places, setPlace }) {
 
     $(function () {
       let list = document.getElementsByClassName("List-Item");
+      let arrowList = document.getElementsByClassName("arrow_image");
       let i;
 
       for (i = 0; i < list.length; i++) {
-        list[i].addEventListener("click", function () {
+        arrowList[i].addEventListener("click", function () {
           this.classList.toggle("active");
           var listImage = this.nextElementSibling;
           if (listImage.style.visibility === "visible") {
@@ -100,10 +108,14 @@ function List({ places, setPlace }) {
   const list = listItem.map((item, i) =>
     <div className='List-Item-div'>
       <p className='List-Item'>
-        {icon}&nbsp;[{item.name}]&nbsp;
-        {isChecked[i] ? <HeartFilled onClick={(event) => toggleLike(event, i)} /> : <HeartOutlined onClick={(event) => toggleLike(event, i)} /> /* true면 꽉 찬 하트, false면 빈 하트 */}&nbsp;{item.like}&nbsp;
-        {/*item.star*/}  {item.explanation}
+        {icon}&nbsp;
+        [{item.name}]<br></br><br></br>
+        {item.explanation}
+        <p style={{display: "inline-block", float: "right"}}>{isChecked[i] ? <HeartFilled onClick={(event) => toggleLike(event, i)} /> : <HeartOutlined onClick={(event) => toggleLike(event, i)} /> /* true면 꽉 찬 하트, false면 빈 하트 */}&nbsp;
+        {item.like}</p>
+        {/* 별점? 저장? item.star*/}
       </p>
+      <img className='arrow_image' src='./images/down-arrow.png' style={{width: "20px", height: "20px"}}/>
       <div className='List-Item-Image-div'>
         <img className='List-Item-Image' src={item.image} alt={item.name} onClick={() => setPlace([item])} />
         <p>[ {item.name} ]</p><p>📍 {item.address}</p>🌐 <a href={item.kakao_map}> {item.kakao_map}</a>
