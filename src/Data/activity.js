689cbc892093308fe07576dc061b4858e1daba7c
newJunsung <assd1108@naver.com>
2022-11-23 16:54:57 +0900

    activity 자료 추가

--- src/Data/activity.js
+++ src/Data/activity.js
@@ -8,7 +8,8 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/1868828759",
         keyword: ["카트", "테마파크"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "트랜디한 공간을 누비며 안에서 놀자!"
     },
     {
         name: "중문오프로드체험장",
@@ -19,7 +20,8 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/2049204914",
         keyword: ["ATV", "버기카"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "한여름의 스트레스를 날려버릴수 있는곳"
     },
     {
         name: "그랑블루 요트투어",
@@ -30,7 +32,8 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/21500202",
         keyword: ["요트", "돌고래"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "🌀🌀또🌀물보라를🌀일으켜🌀🌀 ..다..🐬..다..🐬..다..🐬..다...🐬...다다다..🐬...다다다..."
     },
     {
         name: "제주레포츠랜드",
@@ -41,7 +44,8 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/11714326",
         keyword: ["카트", "짚라인"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "국제경기를 할 수 있는 국내 최대 규모의 카트장"
     },
     {
         name: "김녕미로공원",
@@ -52,7 +56,8 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/17110921",
         keyword: ["아이들", "공원"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "오늘은 내가 테세우스"
     },
     {
         name: "윈드1947카트테마파크",
@@ -63,7 +68,116 @@ const activity = [
         kakao_map: "https://place.map.kakao.com/820826091",
         keyword: ["서바이벌", "카트"],
         like: 0,
-        star: 0.0
+        star: 0.0,
+        explanation: "총과 차를 즐길 수 있는 곳, 동시에 즐기는 건 무리."
+    },
+    {
+        name: "제주스쿠버스쿨",
+        latitude: 33.248470141825614,
+        longitude: 126.52884626326131,
+        address: "제주특별자치도 서귀포시 서호호근로46번길 68",
+        image: "./images/activity/제주스쿠버스쿨.jpg",
+        kakao_map: "https://place.map.kakao.com/1994825891",
+        keyword: ["바다", "스쿠버다이빙"],
+        like: 0,
+        star: 0.0,
+        explanation: "제주 바다의 생태계를 몸으로 느껴보자!"
+    },
+    {
+        name: "서귀포잠수함",
+        latitude: 33.23930334796222,
+        longitude: 126.55860746924927,
+        address: "제주특별자치도 서귀포시 남성중로 40",
+        image: "./images/activity/서귀포잠수함.jpg",
+        kakao_map: "https://place.map.kakao.com/7940952",
+        keyword: ["바다", "잠수함"],
+        like: 0,
+        star: 0.0,
+        explanation: "관광잠수함 '지야호'와 함께하는 해저관광"
+    },
+    {
+        name: "더마파크",
+        latitude: 33.354123798743665,
+        longitude: 126.24444970646005,
+        address: "제주특별자치도 제주시 한림읍 월림리 2365",
+        image: "./images/activity/더마파크.jpg",
+        kakao_map: "https://place.map.kakao.com/11470870",
+        keyword: ["승마", "체험"],
+        like: 0,
+        star: 0.0,
+        explanation: "승마 뿐만이 아니라 공연도 즐길 수 있는 곳"
+    },
+    {
+        name: "송정농원",
+        latitude: 33.48002325111055,
+        longitude: 126.564650782193,
+        address: "제주특별자치도 제주시 연신로 293-13 (우)63308",
+        image: "./images/activity/송정농원.jpg",
+        kakao_map: "https://place.map.kakao.com/20203623",
+        keyword: ["체험", "감귤"],
+        like: 0,
+        star: 0.0,
+        explanation: "어이 김씨... 여기와서 감귤 좀 따!"
+    },
+    {
+        name: "사려니숲길",
+        latitude: 33.42346688637592,
+        longitude: 126.62636213789615,
+        address: "제주특별자치도 제주시 조천읍 교래리 산 137-1",
+        image: "./images/activity/사려니숲길.jpg",
+        kakao_map: "https://place.map.kakao.com/26319779",
+        keyword: ["숲길", "산책"],
+        like: 0,
+        star: 0.0,
+        explanation: "숲 속에서 폐만 맑아지는 것이 아닌, 눈도 맑아진다."
+    },
+    {
+        name: "제주레일바이크",
+        latitude: 33.464621157280874,
+        longitude: 126.83694127878235,
+        address: "제주특별자치도 제주시 구좌읍 용눈이오름로 641 (우)63354",
+        image: "./images/activity/제주레일바이크.jpg",
+        kakao_map: "https://place.map.kakao.com/21777957",
+        keyword: ["경치", "레일"],
+        like: 0,
+        star: 0.0,
+        explanation: "실기평가 조별과제"
+    },
+    {
+        name: "제주라프",
+        latitude: 33.48677409422022,
+        longitude: 126.70688478029275,
+        address: "제주특별자치도 제주시 조천읍 선교로 115-1 (우)63341",
+        image: "./images/activity/제주라프.jpg",
+        kakao_map: "https://place.map.kakao.com/1986965415",
+        keyword: ["체험", "짚라인"],
+        like: 0,
+        star: 0.0,
+        explanation: "제주의 종합 플레이 그라운드"
+    },
+    {
+        name: "하늘을날다",
+        latitude: 33.35634076982624,
+        longitude: 126.29645961169517,
+        address: "제주특별자치도 제주시 한림읍 한창로 1295-1 (우)63019",
+        image: "./images/activity/하늘을날다.jpg",
+        kakao_map: "https://place.map.kakao.com/921333808",
+        keyword: ["하늘", "페더글라이딩"],
+        like: 0,
+        star: 0.0,
+        explanation: "마른하늘을 달려~ 워어어"
+    },
+    {
+        name: "삼다수목장",
+        latitude: 33.432406305678455,
+        longitude: 126.66518597106304,
+        address: "제주특별자치도 제주시 조천읍 교래리 산 93",
+        image: "./images/activity/삼다수목장.jpg",
+        kakao_map: "https://place.map.kakao.com/1032410621",
+        keyword: ["경치", "승마"],
+        like: 0,
+        star: 0.0,
+        explanation: "제주의 자연환경을 느낄 수 있는 목장"
     }
 ];
 
